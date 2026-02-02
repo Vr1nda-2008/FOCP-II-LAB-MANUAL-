4439839357a2bfcaed2dcf386010d315e94c77b7
Vr1nda-2008 <vrindag0730@gmail.com>
2026-02-02 20:28:32 +0530

    Add files via upload

---
+++ C_code.c
@@ -0,0 +1,19 @@
+#include <stdio.h> //i= input o= output (header file of c lang)
+#include <conio.h> //console screen (if want to change the function of console screen)
+
+// #include is a preprocessor directive
+
+int main()
+{
+
+    printf("hellow world!");
+    scanf("enter a number:");
+    const double pi = 3.14;
+    printf("%f\n", pi);
+
+    int x;
+    scanf("%d", &x);
+
+
+
+}
---
+++ cont.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+using namespace std;
+const double pi = 3.14;     //constant variable
